4f2c446aa7cfdfa273ec2e97d985de9ee7234e14
Eren Kabakçı <erenkabakci@gmail.com>
2024-06-20 17:01:38 +0200

    Update Package.swift

--- wheel/Package.swift
+++ wheel/Package.swift
@@ -10,14 +10,11 @@ let package = Package(
     .macOS(.v11)
   ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "wheel",
       targets: ["wheel"]),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "wheel"),
     .testTarget(
